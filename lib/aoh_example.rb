e5c9c70186a9435c385707e71ebee58144f1a49d
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-26 18:41:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,6 +24,7 @@ def literal_aoh
    { name:  "Don Gately", occupation: "Live-in Staff" },
    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
    
+   
    ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
